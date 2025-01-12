0d046a4af9b9efeb9b738a250e0be55457bed77c
Eliyahu Masinter <eliyahumasinter@gmail.com>
2025-01-12 14:33:24 +0200

    Introduce error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //First element is executable file path
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!(
         "Searching for query {} in file {}",
@@ -20,9 +23,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
