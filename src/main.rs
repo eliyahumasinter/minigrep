fffe06f8b01743fe634c0ff7e32840f681fe52c6
Eliyahu Masinter <eliyahumasinter@gmail.com>
2025-01-12 15:54:16 +0200

    TDD and logic implementation

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!(
-        "Searching for query {} in file {}",
-        config.query, config.file_path
-    );
     if let Err(e)= minigrep::run(config) {
         println!("Application error {e}");
         process::exit(1);
